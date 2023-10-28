e93fb5de1af9f7a76d4f7971a436077769ee0c0e
William Harrison <william@williamdavidharrison.com.au>
2023-10-28 13:20:03 +0800

    Update discord-status.js

--- content/discord-status.js
+++ content/discord-status.js
@@ -1,16 +1,38 @@
-fetch("https://api.lanyard.rest/v1/users/853158265466257448")
-    .then((res) => res.json()).then((res) => {
-        switch (res.data.discord_status) {
-            case "online":
-                document.getElementById("pfp").style.border = "4px solid #46A55F";
-                break;
-            case "idle":
-                document.getElementById("pfp").style.border = "4px solid #F5A626";
-                break;
-            case "dnd":
-                document.getElementById("pfp").style.border = "4px solid #E74044";
-                break;
-            default:
-                document.getElementById("pfp").style.border = "4px solid #757F8D";
-        }
-    })
+try {
+    fetch("https://lanyard-api.wdh.gg/v1/users/853158265466257448")
+        .then((res) => res.json()).then((res) => {
+            switch (res.data.discord_status) {
+                case "online":
+                    document.getElementById("pfp").style.border = "4px solid #46A55F";
+                    break;
+                case "idle":
+                    document.getElementById("pfp").style.border = "4px solid #F5A626";
+                    break;
+                case "dnd":
+                    document.getElementById("pfp").style.border = "4px solid #E74044";
+                    break;
+                default:
+                    document.getElementById("pfp").style.border = "4px solid #757F8D";
+            }
+        })
+} catch(err) {
+    console.error(err);
+
+    // Default back the regular Lanyard API
+    fetch("https://api.lanyard.rest/v1/users/853158265466257448")
+        .then((res) => res.json()).then((res) => {
+            switch (res.data.discord_status) {
+                case "online":
+                    document.getElementById("pfp").style.border = "4px solid #46A55F";
+                    break;
+                case "idle":
+                    document.getElementById("pfp").style.border = "4px solid #F5A626";
+                    break;
+                case "dnd":
+                    document.getElementById("pfp").style.border = "4px solid #E74044";
+                    break;
+                default:
+                    document.getElementById("pfp").style.border = "4px solid #757F8D";
+            }
+        })
+}
